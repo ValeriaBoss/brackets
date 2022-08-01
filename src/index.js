a5c55b330815a2fd0fb0fb26fe62e7ab480b305c
ValeriaBoss <pokhylko.valeria@gmail.com>
2022-08-01 19:58:56 +0200

    add: new information

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str,bracketsConfig) {
+  let OPEN_BRACKETS = [];
+  let BRACKETS_PAIR = {};
+ 
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    OPEN_BRACKETS.push(bracketsConfig[i][0]);
+    BRACKETS_PAIR[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  let stack = [];
+ 
+  for(let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+      if ((stack[stack.length - 1] === currentSymbol) && (BRACKETS_PAIR[currentSymbol] === currentSymbol)) {
+        stack.pop();
+      }
+    else {
+      stack.push(currentSymbol)
+    }
+   }
+    else {
+      if (stack.length === 0) {
+        return  false;
+      }
+      let topElement = stack[stack.length - 1];
+
+      if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
